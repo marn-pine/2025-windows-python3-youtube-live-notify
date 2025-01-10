8c792798fe81f024172cb9c10927ccab4453f28f
marn-pine <marn.pinescript@gmail.com>
2025-01-10 15:06:23 +0700

    10-Jan-25

--- readme.txt
+++ readme.txt
@@ -1,4 +1,5 @@
-# latest update 10-Jan-25, for Windows 11 from Python 3.1.2 
-# ready compiled stand-alone .exe file with .ini file
-# this app will loop notify when youtube channel live 
-# right click tray icon and choose exit to exit app 
+# Latest update: 10-Jan-25, for Windows 11 and from Python 3.1.2 
+# Ready-compiled stand-alone .exe file used with .ini file
+# This app will notify you when a YouTube channel goes live. 
+# Right-click the tray icon and choose Exit to exit the app.
+# .exe file can copy to new file, .ini must be the same name 
--- youtube-live-notify.py
+++ youtube-live-notify.py
@@ -1,13 +1,15 @@
-# latest update 10-Jan-25, for Windows 11 from Python 3.1.2 
-# ready compiled stand-alone .exe file with .ini file
-# this app will loop notify when youtube channel live 
-# right click tray icon and choose exit to exit app                                                    
+# Latest update: 10-Jan-25, for Windows 11 and from Python 3.1.2 
+# Ready-compiled stand-alone .exe file used with .ini file
+# This app will notify you when a YouTube channel goes live. 
+# Right-click the tray icon and choose Exit to exit the app.
+# .exe file can copy to new file, .ini must be the same name                                                    
 
-# internal packages
+# built-in packages
 from time import *
 from PIL import Image
 from configparser import ConfigParser
 import threading
+import os
 import pystray
 import winsound
 
@@ -37,8 +39,11 @@ def after_click(icon, query):
         icon.stop()
         
 # read ini file
+app_name = os.path.splitext(os.path.basename(__file__))[0]
+ini_name = app_name + ".ini"
 configur = ConfigParser() 
-configur.read('youtube-live-notify.ini')
+configur.read(ini_name)
+
 live_channel = configur.get('Live Channel','Live')
 notify_sound = configur.get('Notify Sound','Wav')
 tray_icon = configur.get('Tray Icon','Png')
@@ -52,4 +57,5 @@ timer_thread.start()
 image = Image.open("youtube-live-notify.png") 
 icon = pystray.Icon("youtube-live-notify", image, "youtube-live-notify",menu=pystray.Menu(
        pystray.MenuItem("Exit", after_click)))
+
 icon.run()
